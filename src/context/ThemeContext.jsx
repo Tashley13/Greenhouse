d363bc96efe427aab92f2e04a83f194ef940adc9
Taylor <taylorashley1304@gmail.com>
2024-05-15 18:42:44 -0400

    phase 0

---
+++ src/context/ThemeContext.jsx
@@ -0,0 +1,22 @@
+import { createContext, useContext, useState } from 'react';
+
+
+export const ThemeContext = createContext();
+
+export const useTheme = () => useContext(ThemeContext);
+
+export default function ThemeProvider({ children }) {
+  const [themeName, setThemeName] = useState("night");
+
+
+  return (
+    <ThemeContext.Provider
+      value={{
+        themeName,
+        setThemeName
+      }}
+    >
+      {children}
+    </ThemeContext.Provider>
+  );
+}
