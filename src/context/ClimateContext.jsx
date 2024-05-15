7891017737eaba485686e68fc528ad7af1cc50cb
Taylor <taylorashley1304@gmail.com>
2024-05-15 19:18:53 -0400

    phase 3

--- src/context/ClimateContext.jsx
+++ src/context/ClimateContext.jsx
@@ -1,2 +1,21 @@
+import { createContext, useContext, useState } from "react";
 // Temperature has a default value of 50 degrees
 // Humidity has a default value of 40%
+export const ClimateContext=createContext();
+
+export const useClimate = () => useContext(ClimateContext);
+
+export default function ClimateProvider({children}) {
+    const [ greenhouseTemp, setGreenhouseTemp] = useState(50);
+
+    return (
+        <ClimateContext.Provider
+        value = {{
+            greenhouseTemp,
+            setGreenhouseTemp
+        }}
+        >
+            {children}
+        </ClimateContext.Provider>
+    )
+}
