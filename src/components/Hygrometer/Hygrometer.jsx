2e7e661288f87f9ad0d2bfe09cc7b304f71e1ce8
Taylor <taylorashley1304@gmail.com>
2024-05-15 20:58:21 -0400

    phase 6 bonuses

--- src/components/Hygrometer/Hygrometer.jsx
+++ src/components/Hygrometer/Hygrometer.jsx
@@ -1,14 +1,38 @@
 import ReactSlider from 'react-slider';
+import { useEffect, useState } from 'react';
 import './Hygrometer.css';
 import { useClimate } from '../../context/ClimateContext';
 
+
 function Hygrometer() {
   //pull the humidity values from the imported useClimate
-  const { greenhouseHumidity, setGreenhouseHumidity} = useClimate();
+  const { greenhouseHumidity, setGreenhouseHumidity } = useClimate();
+  //create a use state to keep track of the changing humidity
+  //set it equal to the current humidity
+  const [humidity, setHumidity] = useState(greenhouseHumidity);
+
+  useEffect(() => {
+    const humidityAdjuster = () => {
+      if (greenhouseHumidity > humidity) {
+        setHumidity(prevHumidity => prevHumidity + 1)
+      }
+      if (greenhouseHumidity < humidity) {
+        setHumidity(prevHumidity => prevHumidity - 1)
+      }
+    }
+
+    if (greenhouseHumidity != humidity) {
+      const timer = setTimeout(humidityAdjuster, 1000);
+      return () => clearTimeout(timer)
+    }
+
+  }, [greenhouseHumidity, humidity, setHumidity])
+
+
   return (
     <section>
       <h2>Hygrometer</h2>
-      <div className="actual-humid">Actual Humidity: {greenhouseHumidity}%</div>
+      <div className="actual-humid">Actual Humidity: {humidity}%</div>
       <ReactSlider
         value={greenhouseHumidity}
         onAfterChange={(val) => setGreenhouseHumidity(val)}
--- src/components/Thermometer/Thermometer.jsx
+++ src/components/Thermometer/Thermometer.jsx
@@ -1,14 +1,40 @@
 import ReactSlider from 'react-slider';
+import { useEffect, useState } from 'react';
 import './Thermometer.css';
 import { useClimate } from '../../context/ClimateContext';
 
 function Thermometer() {
   //pull the climate temps
   const {greenhouseTemp, setGreenhouseTemp} = useClimate();
+  //desired temp useState, set it equal to the current temp
+  const [temp, setTemp]= useState(greenhouseTemp);
+
+  useEffect(()=> {
+    const temperatureAdjuster = () => {
+      //add 1 if the chosen temp is greater than the current
+      if (greenhouseTemp > temp) {
+        setTemp(prevTemp=> prevTemp +1)
+      }
+      //subtract 1 if the chosen temp is less than the current
+      if (greenhouseTemp <temp) {
+        setTemp(prevTemp=> prevTemp -1)
+      }
+    };
+
+    //timer to check if the temperatures are equal
+    if(greenhouseTemp!= temp) {
+      //creating a variable to clear in the return below
+      const timer=setTimeout(temperatureAdjuster, 1000);
+      return() =>clearTimeout(timer)
+    }
+    //keep an eye on:
+  }, [greenhouseTemp, temp, setTemp])
+  //Actual temperature will be the temp useState as it will be changing
+  //each time in the useEffect
   return (
     <section>
       <h2>Thermometer</h2>
-      <div className="actual-temp">Actual Temperature: {greenhouseTemp}°F</div>
+      <div className="actual-temp">Actual Temperature: {temp}°F</div>
       <ReactSlider
         value={greenhouseTemp}
         onAfterChange={(val) => setGreenhouseTemp(val)}
